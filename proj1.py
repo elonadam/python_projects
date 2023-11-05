ead5b5a0fd375ada90eaa72dcd0b66c7615d081a
elon <76097179+elonadam@users.noreply.github.com>
2023-11-06 00:17:29 +0200

    Update proj1.py

--- proj1.py
+++ proj1.py
@@ -5,8 +5,8 @@
 
 print("Hello and welcome to my 1st project in 100 days of code challenge !\nIt's a band name generator")
 
-city_name = input("Which city did you grow up in ?")
+city_name = input("Which city did you grow up in \n?")
 
-pet_name = input("what is your pet name ? ")
+pet_name = input("what is your pet name \n? ")
 
 print("your new band can be called " +city_name +" "+ pet_name +" !!")
