41856171e06c1b0a223d689b459f28ab0cc91660
elon <76097179+elonadam@users.noreply.github.com>
2023-11-09 18:27:38 +0200

    Create proj6 - reeborg's world sol.py

---
+++ proj6 - reeborg's world sol.py
@@ -0,0 +1,32 @@
+# 6/100 days of coding, here are 2 different maps solutions to Reeborg's world
+
+
+# solution to Hardle 1-4
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+        turn_right()
+        if front_is_clear():
+            move()
+            turn_right()
+            while front_is_clear():
+                move()
+            turn_left()
+    if at_goal():
+        break
+    if front_is_clear():
+        move()
+
+
+#solution for maze map
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
