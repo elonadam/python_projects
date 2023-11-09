67ac8b6e03ef4a3db3621385115c9a4c9c736806
elon <76097179+elonadam@users.noreply.github.com>
2023-11-09 18:46:16 +0200

    Update proj6 - reeborg's world sol.py

--- proj6 - reeborg's world sol.py
+++ proj6 - reeborg's world sol.py
@@ -30,3 +30,28 @@ while not at_goal():
 
 #solution for maze map
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+
+while not is_facing_north():
+        turn_left()    
+turn_right()
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif not front_is_clear() and right_is_clear():
+        turn_right()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+        
+        
