75bba07521bb93572c7bc3ed167727ce2220320f
elon <76097179+elonadam@users.noreply.github.com>
2023-11-12 20:35:45 +0200

    Update proj8 - caeser cipher.py

--- proj8 - caeser cipher.py
+++ proj8 - caeser cipher.py
@@ -1,5 +1,11 @@
-# to add option to insert full sentences
 
+
+# to add option to insert full sentences
+#e.g. 
+#plain_text = "hello"
+#shift = 5
+#cipher_text = "mjqqt"
+#print output: "The encoded text is mjqqt"
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 logo = """           
  ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
@@ -28,13 +34,6 @@ while user_answer == 'yes':
   text = input("Type your message:\n").lower()
   shift = int(input("Type the shift number:\n"))
   
-  #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-  #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-  #e.g. 
-  #plain_text = "hello"
-  #shift = 5
-  #cipher_text = "mjqqt"
-  #print output: "The encoded text is mjqqt"
   def encrypt_decrypt (text_list, shift):
     encrypt_text = ""
     if direction == 'decode':
