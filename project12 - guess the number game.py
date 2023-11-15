855db6b1066381a20f0728a0722384510403bbbb
elon <76097179+elonadam@users.noreply.github.com>
2023-11-15 22:24:10 +0200

    Update project12

--- project12 - guess the number game.py
+++ project12 - guess the number game.py
@@ -12,7 +12,7 @@ U /"___|U |"|u| \| ___"|/ __"| / __"| u      |_ " _| |'| |'|\| ___"|/     | \ |"
 
 '''
 import random
-#Number Guessing Game Objectives:
+
 def binary_search(target_num, input_num):
   if target_num == input_num:
     print(f"You Win ! the number indeed was {target_num}")
@@ -21,14 +21,6 @@ def binary_search(target_num, input_num):
     print("Too High !\nGuess again")
   else:
     print("Too Low !\nGuess again")  
-
-    
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
 print (logo)
 
