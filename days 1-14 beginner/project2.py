fbd899bcd24a9de547ad463edd4d1c233716dbc1
elon <76097179+elonadam@users.noreply.github.com>
2023-11-19 23:30:13 +0200

    Update project2.py

--- days 1-14 beginner/project2.py
+++ days 1-14 beginner/project2.py
@@ -1,9 +1,9 @@
-# Second project during 100 days of coding challange, it's a tip calculator 
+# Second project during 100 days of coding challenge, it's a tip calculator 
 
 print("Welcome to the tip calculator !\n")
 bill_amount = input("What is the total bill ?\n")
-precentage = input(
-    "How much tip you want to add ? choose a number between 10-20\n")
+percentage = input(
+    "How much tip do you want to add? choose a number between 10-20\n")
 people_count = input("How many persons was dinning ?\n")
-result = int(bill_amount) * (int(precentage) /100 + 1 ) / int(people_count)
+result = int(bill_amount) * (int(percentage) /100 + 1 ) / int(people_count)
 print(f"The amount to pay for each person is: {round(result, 2)} !\n Have a nice day !")
