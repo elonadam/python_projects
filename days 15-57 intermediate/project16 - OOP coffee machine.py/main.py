4cd84c7b10cc5aab588e5db4142c80c674500899
elon <76097179+elonadam@users.noreply.github.com>
2023-11-19 23:56:32 +0200

    Update main.py

--- days 15-57 intermediate/project16 - OOP coffee machine.py/main.py
+++ days 15-57 intermediate/project16 - OOP coffee machine.py/main.py
@@ -3,11 +3,10 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 # TODO 1 print report
-# TODO 2 check resour
+# TODO 2 Check resources
 # TODO 3 process coins|
 # TODO 4 check transaction
 # TODO 5 make coffee
-
 
 my_money_machine = MoneyMachine()  # create object of class MoneyMachine
 my_coffee_maker = CoffeeMaker()
@@ -27,7 +26,6 @@ while is_on:
         coffee = menu.find_drink(user_choice)
         if my_coffee_maker.is_resource_sufficient(coffee):
             print(f"cost of {coffee.name} is: {coffee.cost}")
-            my_money_machine.make_payment(coffee.cost)
-            
-            my_coffee_maker.make_coffee(coffee)
+            if my_money_machine.make_payment(coffee.cost):
+                my_coffee_maker.make_coffee(coffee)
 
