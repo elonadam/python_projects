a118236f6a9d39616dbff7bf61c1f898420cb7c1
elon <76097179+elonadam@users.noreply.github.com>
2023-11-19 23:50:13 +0200

    Create main.py

---
+++ days 15-57 intermediate/project16 - OOP coffee machine.py/main.py
@@ -0,0 +1,33 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# TODO 1 print report
+# TODO 2 check resour
+# TODO 3 process coins|
+# TODO 4 check transaction
+# TODO 5 make coffee
+
+
+my_money_machine = MoneyMachine()  # create object of class MoneyMachine
+my_coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+while is_on:
+
+    options = menu.get_items()
+    user_choice = input(f"What would you like? {options}")
+    if user_choice == "off":
+        is_on = False
+    elif user_choice == "report":
+        my_money_machine.report()  # its like car.speed()
+        my_coffee_maker.report()
+    else:
+        coffee = menu.find_drink(user_choice)
+        if my_coffee_maker.is_resource_sufficient(coffee):
+            print(f"cost of {coffee.name} is: {coffee.cost}")
+            my_money_machine.make_payment(coffee.cost)
+            
+            my_coffee_maker.make_coffee(coffee)
+
