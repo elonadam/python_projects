5519a583612ae27934bcaa8f59156e6012a692fa
elon <76097179+elonadam@users.noreply.github.com>
2023-11-22 21:21:20 +0200

    Update project19 - turtle race.py

--- days 15-57 intermediate/project19 - turtle race.py
+++ days 15-57 intermediate/project19 - turtle race.py
@@ -1,1 +1,42 @@
 # 19/100, turtles race based on Turtle graphics
+
+
+import random
+import turtle
+from turtle import Turtle, Screen
+
+all_turtles = []
+
+screen = Screen()
+screen.setup(width=500,height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race ? Enter a color:")
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+start_y = -100
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=start_y)
+    start_y += 40
+    all_turtles.append(new_turtle)
+
+is_race_on = False
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 220:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! the {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! the {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
+
