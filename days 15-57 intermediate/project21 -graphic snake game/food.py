f2bbd8d6eb2a8c585d5db46089cc71faa9c1a696
elon <76097179+elonadam@users.noreply.github.com>
2023-11-25 00:52:51 +0200

    Update food.py

--- days 15-57 intermediate/project21 -graphic snake game/food.py
+++ days 15-57 intermediate/project21 -graphic snake game/food.py
@@ -1,1 +1,21 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # creating 10X10 circle
+        self.color("yellow")
+        self.speed("fastest")
+        self.food_relocated()
+
+    def food_relocated(self):
+        """generate new food in random location"""
+        random_x = random.randint(-270, 270)
+        random_y = random.randint(-270, 270)
+        self.goto(random_x, random_y)
 
