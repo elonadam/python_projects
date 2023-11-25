36e884e3d00bdeabfb83840841da36012fa8c576
Elon <76097179+elonadam@users.noreply.github.com>
2023-11-25 15:53:26 +0200

    Update main.py

--- days 15-57 intermediate/project22 - pong game/main.py
+++ days 15-57 intermediate/project22 - pong game/main.py
@@ -1,1 +1,46 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)  # tracer off
+
+ball = Ball()
+scoreboard = Scoreboard()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.up, key="Up")
+screen.onkey(r_paddle.down, key="Down")
+screen.onkey(l_paddle.up, key="w")
+screen.onkey(l_paddle.down, key="s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move(False)
+
+    # Detect collision with top/bottom
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.y_bounce()
+
+    # Detect collision with a paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+
+    # Detect paddle miss
+    elif ball.xcor() > 450 or ball.xcor() < -450:
+        scoreboard.increase_score(ball.xcor())
+        ball.reset_position()
+
+
+screen.exitonclick()
 
