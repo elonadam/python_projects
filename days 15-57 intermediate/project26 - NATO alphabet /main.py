088d03693c9b3e202a0c6105b8dc1d309aeac1ef
Elon <76097179+elonadam@users.noreply.github.com>
2023-12-09 23:16:40 +0200

    Update main.py

--- days 15-57 intermediate/project26 - NATO alphabet /main.py
+++ days 15-57 intermediate/project26 - NATO alphabet /main.py
@@ -1,34 +1,12 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# #Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     #Access key and value
-#     pass
-
+# given a certain word this code prints a list of NATO alphabet word to help pronunce the letters
+#e.g alpha for a, zulu for z
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_df = pandas.DataFrame(data)
-# print(alphabet_df)
-# # Loop through rows of a data frame
-# for (index, row) in alphabet_df.iterrows():
-#      print(row.letter,row.code)
 
-#     # Access row.student or row.score
-#     pass
 alphabet_dict = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
-print(alphabet_dict)
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-# alphabet_dict = {letter: code for (letter, code) in alphabet_df.iterrows()}
-# print(alphabet_dict)
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 user_word = input("give a word: ").upper()
 user_word_list = [letter for letter in user_word] # generate a separate char list form user word
 
