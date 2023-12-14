b281d2c1d2e10196975ef44fa5215384f88e6753
Elon <76097179+elonadam@users.noreply.github.com>
2023-12-14 12:20:10 +0200

    Update main.py

--- days 15-57 intermediate/project29 - GUI password manager/main.py
+++ days 15-57 intermediate/project29 - GUI password manager/main.py
@@ -1,5 +1,3 @@
-
-
 from tkinter import *
 
 GREY = '#BBBBBB'
@@ -12,10 +10,16 @@ def generate_pass():
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_pass():
-    pass
 
+# TODO 1 save the user input to data.txt file by clicking the add button
+# TODO 2 to clear the input after add button pressed
 
+def save_to_file():
+    """Getting tuple input and append that in a text file"""
+    user_set = add_pass()  # get input
+    with open("C:/Users/USER/Documents/pycharmProjects/day29/data.txt", "a") as file:
+        file.write(user_set)
+    clear_input(website_input)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -39,28 +43,41 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:", bg=LIGHT_GREY, highlightthickness=0)
 password_label.grid(row=3, column=0)
 
-# Buttons
-
-generate_button = Button(text="Generate Passwords", bg=LIGHT_GREY, width=15, command=generate_pass)
-generate_button.grid(row=3, column=2)
-
-add_button = Button(text="Add", bg=LIGHT_GREY, width=30, command=add_pass)
-add_button.grid(row=4, column=1, columnspan=2)
-
 # Entries
 
 website_input = Entry(width=35)
-wev_data = website_input.get()
+# web_data = website_input.get()
 website_input.grid(row=1, column=1, columnspan=2)
 website_input.focus()
 
 email_input = Entry(width=35)
 email_input.insert(END, string="elon@python3.com")
-email_data = email_input.get()
+# email_data = email_input.get()
 email_input.grid(row=2, column=1, columnspan=2)
 
 password_input = Entry(width=16)
-pass_data = password_input.get()
+# pass_data = password_input.get()
 password_input.grid(row=3, column=1, columnspan=1)
+
+
+# Buttons
+def clear_input(entry_name):
+    entry_name.delete(0, END)
+
+
+def add_pass():
+    web_data = website_input.get() # Get input from the entry field
+    email_data = email_input.get()
+    pass_data = password_input.get()
+    new_set = f"{web_data} | {email_data} | {pass_data} \n" # Storing collected data in tuple
+    #print("from add_pass", new_set)
+    return new_set
+
+
+generate_button = Button(text="Generate Passwords", bg=LIGHT_GREY, width=15, command=generate_pass)
+generate_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", bg=LIGHT_GREY, width=30, command=save_to_file)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
