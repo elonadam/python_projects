95621806d7d2582e95b1681f461f531bd52d150a
Elon <76097179+elonadam@users.noreply.github.com>
2023-12-18 01:26:02 +0200

    Update main.py

--- days 15-57 intermediate/project29 - GUI password manager/main.py
+++ days 15-57 intermediate/project29 - GUI password manager/main.py
@@ -1,7 +1,12 @@
+#29/100 days of code, was edited on the 30th day, added search for passwords and change the data file form txt to json
+
+
 from tkinter import *
 from tkinter import messagebox
 from random import randint, shuffle, choice
 import pyperclip
+import json
+
 LIGHT_GREY = '#E2D5D5'
 
 
@@ -22,11 +27,12 @@ def generate_password():
     pass_list = letter_list + number_list + sym_list
     shuffle(pass_list)  # Order of characters randomised:
 
-    final_pass = "".join(pass_list) # Adds all the pieces to a string
+    final_pass = "".join(pass_list)  # Adds all the pieces to a string
 
     print(f"Your password is: " + final_pass)
     password_input.insert(0, final_pass)
-    pyperclip.copy(final_pass) # Copy the password to the computer clipboard
+    pyperclip.copy(final_pass)  # Copy the password to the computer clipboard
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -35,12 +41,28 @@ def clear_input(*args):
         entry.delete(0, END)
 
 
+small_data = {
+    "start_value": {
+        "email": "elon@python.com",
+        "password": "goodPassword"
+    }
+}
+
+with open("data.json", "w") as data_file:
+    json.dump(small_data, data_file, indent=4)
+
+
 def save_to_file():
     """Getting a string input and append that in a text file"""
     web_data = website_input.get()  # Get input from the entry field
     email_data = email_input.get()
     pass_data = password_input.get()
-
+    new_data = {
+        web_data: {
+            "email": email_data,
+            "password": pass_data,
+        }
+    }
     if len(web_data) == 0 or len(email_data) == 0 or len(pass_data) == 0:
         messagebox.showerror(title="Error", message="Please fill all the fields")
     else:
@@ -49,9 +71,40 @@ def save_to_file():
                                                       f" entered:\nEmail: {email_data}\nPassword: {pass_data}\nDo you want to save it ?")
 
         if want_to_save:  # this argument has boolean value based on what was clicked in the alert box
-            with open("data.txt", "a") as file:
-                file.write(f"{web_data} | {email_data} | {pass_data} \n")  # get input from func and append it
-            clear_input(website_input, password_input)
+            with open("data.json", "a") as file:
+                try:
+                    with open("data.json", "r") as data_file:
+                        # Reading old data
+                        data = json.load(data_file)
+                except FileNotFoundError:
+                    with open("data.json", "w") as data_file:
+                        json.dump(new_data, data_file, indent=4)
+                else:
+                    # Updating old data with new data
+                    data.update(new_data)
+
+                    with open("data.json", "w") as data_file:
+                        # Saving updated data
+                        json.dump(data, data_file, indent=4)
+                finally:
+                    clear_input(website_input, password_input)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_input.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -78,23 +131,26 @@ password_label.grid(row=3, column=0)
 
 # Entries
 
-website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=16)
+website_input.grid(row=1, column=1)
 website_input.focus()
 
 email_input = Entry(width=35)
-email_input.insert(END, string="elon@python3.com")
 email_input.grid(row=2, column=1, columnspan=2)
+email_input.insert(END, string="elon@python3.com")
 
 password_input = Entry(width=16)
-password_input.grid(row=3, column=1, columnspan=1)
+password_input.grid(row=3, column=1)
 
 # Buttons
 
-generate_button = Button(text="Generate Passwords", bg=LIGHT_GREY, width=15, command=generate_password)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+
+generate_button = Button(text="Generate Passwords", width=15, command=generate_password)
 generate_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", bg=LIGHT_GREY, width=30, command=save_to_file)
+add_button = Button(text="Add", width=30, command=save_to_file)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
