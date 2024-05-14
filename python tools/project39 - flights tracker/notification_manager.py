9f6e05ce3076fb512274de45f7975a36ceb11cba
Elon <76097179+elonadam@users.noreply.github.com>
2024-05-14 23:09:54 +0300

    Update notification_manager.py

--- python tools/project39 - flights tracker/notification_manager.py
+++ python tools/project39 - flights tracker/notification_manager.py
@@ -1,20 +1,23 @@
 from twilio.rest import Client
+import os
 
-TWILIO_SID = 'AC43d33746c61b3124371833da9f4f85b5'
-TWILIO_AUTH_TOKEN = '001d564321a27b13cabe7c13a168da7b'
-TWILIO_VIRTUAL_NUMBER = "censored"  # was my number
-TWILIO_VERIFIED_NUMBER = "+14845145064"  # my twilio number
+twilio_sid = os.environ.get('SID')
+auth_token = os.environ.get('AUTHTOKEN')
+twilio_number = "+15709337522"  # my twilio number
+my_number = os.environ.get('MYNUMBER')
+
+
+# print(f"sid is {twilio_sid} auth is {auth_token} my num is {my_number}")
 
 
 class NotificationManager:
     def __init__(self):
-        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+        self.client = Client(twilio_sid, auth_token)
 
     def send_sms(self, message):
         message = self.client.messages.create(
             body=message,
-            from_=TWILIO_VIRTUAL_NUMBER,
-            to=TWILIO_VERIFIED_NUMBER,
+            from_=twilio_number,
+            to=my_number,
         )
-        # Prints if successfully sent.
         print(message.sid)
