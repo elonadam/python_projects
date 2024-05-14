1ae85e14d5d6f8dfb20fbbc86b91f56762ddce66
Elon <76097179+elonadam@users.noreply.github.com>
2024-05-14 23:09:09 +0300

    Update main.py

--- python tools/project39 - flights tracker/main.py
+++ python tools/project39 - flights tracker/main.py
@@ -1,4 +1,5 @@
-
+# searching for low-cost flights, based on flight budget from a google sheet connected as API
+# when a low price flight found, sending me SMS with the details
 
 from datetime import datetime,timedelta
 from data_manager import DataManager
@@ -15,24 +16,25 @@ origin_city_iata = 'TLV'
 tomorrow = (datetime.today() + timedelta(1)).strftime('%d/%m/%Y')
 six_month_from_now = (datetime.today() + timedelta(180)).strftime('%d/%m/%Y')
 
-
 if sheet_data[0]['iataCode'] == '' : # no iata code in sheet
     for city in sheet_data:
         iata_code = flight_search.search_iata(city['city'])  # get iata codes
         city['iataCode'] = iata_code  # set a new iata codes
         index = sheet_data.index(city) + 2
-        data_manager.store_iata_codes(index, iata_code)
+        data_manager.store_iata_codes()
 
-for destination in sheet_data:
+for destination in sheet_data: # search flights for each destination
     flight = flight_search.search_flight(
         origin_city_iata,
         dest_city_iata=destination['iataCode'],
         from_date=tomorrow,to_date=six_month_from_now
     )
+    if flight is None:
+        continue
 
     if flight.price < destination["lowestPrice"]:
         notification_manager.send_sms(
-            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to "
-                    f"{flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to"
-                    f" {flight.return_date}."
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to"
+                    f" {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to"
+                    f" {flight.return_date}.\n click here to purchase {flight.deep_link}"
         )
