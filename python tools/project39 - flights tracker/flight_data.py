e2c5bdde8a581dba36c13a397d86e08687d11241
Elon <76097179+elonadam@users.noreply.github.com>
2024-05-14 23:08:37 +0300

    Update flight_data.py

--- python tools/project39 - flights tracker/flight_data.py
+++ python tools/project39 - flights tracker/flight_data.py
@@ -1,6 +1,6 @@
 class FlightData:
     # This class is responsible for structuring the flight data.
-    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date):
+    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date,deep_link):
         self.price = price
         self.origin_city = origin_city
         self.origin_airport = origin_airport
@@ -8,3 +8,4 @@ class FlightData:
         self.destination_airport = destination_airport
         self.out_date = out_date
         self.return_date = return_date
+        self.deep_link = deep_link
