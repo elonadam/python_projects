aa153e628ad3b112e124b351a397a4a7f0cb0e44
Elon <76097179+elonadam@users.noreply.github.com>
2024-11-17 21:30:24 +0200

    Update category_manager.py

--- cocktail_app to git/category_manager.py
+++ cocktail_app to git/category_manager.py
@@ -1,4 +1,11 @@
 
+# //current have
+# //['Mojito', 'Negroni', 'Rubicon', 'Old Fashioned', 'Martini', 'Margarita', 'Cosmopolitan', 'Daiquiri', 'Whiskey Sour',
+# //'Pina Colada', 'Bellini', 'Black Russian', 'Bloody Mary', 'Caipirinha', 'Champagne Cocktail', 'Corpse Reviver #2',
+# //'French 75', 'Mai Tai', 'Pisco Sour', 'Sazerac', 'Sidecar', 'Tom Collins', 'Vesper', 'Zombie', 'Boulevardier',
+# //'French Martini', 'Hemingway Special', 'Singapore Sling', 'Last Word', 'Paper Plane', 'Spicy Fifty', "Planter's Punch",
+# //'Long Island Iced Tea', 'Porn Star Martini', 'Gin Basil Smash', 'Naked and Famous', 'Amaretto Sour', 'Espresso Martini']
+
 
 class CategoryManager:
     def __init__(self, cocktail_manager):
